23fd36431c37a7d366495992d4eb47ea93435bba
Ganesh Pawar <pawarganeshb123@gmail.com>
2025-02-11 17:51:56 +0530

    had changes

--- MVC_Task/src/main/java/com/app/repository/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entity.ExpenseEntity;
-
-public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Integer> {
-}
